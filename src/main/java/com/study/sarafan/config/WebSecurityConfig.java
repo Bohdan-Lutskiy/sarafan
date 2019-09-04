8616d9ae0e0238d7d3c28fb1ca5739de5c1fbb08
Bohdan Lutskiy <47318375+bum240798@users.noreply.github.com>
2019-09-04 19:02:42 +0300

    add security, user entity and google authorisation (#4)

---
+++ src/main/java/com/study/sarafan/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.study.sarafan.config;
+
+import com.study.sarafan.domain.User;
+import com.study.sarafan.repo.UserDetailRepo;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PrincipalExtractor principalExtractor(UserDetailRepo userDetailRepo){
+        return map -> {
+            String id = (String) map.get("sub");
+            User user = userDetailRepo.findById(id)//we seek user in DB if we didn't find it there, then
+                    .orElseGet(() ->{//go here and create new one
+                User newUser = new User();
+
+                newUser.setId(id);
+                newUser.setName((String) map.get("name"));
+                newUser.setEmail((String) map.get("email"));
+                newUser.setUserpic((String) map.get("picture"));
+                newUser.setLocale((String) map.get("locale"));
+                newUser.setGender((String) map.get("gender"));
+
+                return newUser;
+            });
+
+            user.setLocalVisit(LocalDateTime.now());
+
+            return userDetailRepo.save(user);
+        };
+    }
+}
