fcceb9a244e460711095badb578f3fa44747d57d
monst <monster240798@gmail.com>
2019-09-10 23:40:04 +0300

    Added Vuetify

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
 import { connect } from "./util/ws";
+import vuetify from './plugins/vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-connect()
+if (frontendData.profile){
+    connect()
+}
 
 Vue.use(VueResource)
 
 new Vue({
     el: '#app',
+    vuetify,
     render: a => a(App)
 })
--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,19 +1,25 @@
 <template>
-    <div>
-        <div v-if="!profile">You need login through
-            <a href="/login">Google</a>
-        </div>
-        <div v-else>
-            <div>{{profile.name}}&nbsp;<a href="/logout">Logout</a></div>
-            <messages-list :messages="messages"/>
-        </div>
-    </div>
+    <v-app>
+        <v-app-bar app>
+            Sarafan
+            {{profile.name}}
+            <a href="/logout">Logout</a>
+        </v-app-bar>
+        <v-content>
+            <div v-if="!profile">You need login through
+                <a href="/login">Google</a>
+            </div>
+            <div>
+                <messages-list :messages="messages"/>
+            </div>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
     import MessagesList from "components/messages/MessageList.vue";
-    import { addHandler } from "util/ws";
-    import { getIndex } from "util/collections";
+    import {addHandler} from "util/ws";
+    import {getIndex} from "util/collections";
 
     export default {
         components: {
@@ -28,7 +34,7 @@
         created() {
             addHandler(data => {
                 let index = getIndex(this.messages, data.id)
-                if (index > -1){
+                if (index > -1) {
                     this.messages.splice(index, 1, data)
                 } else {
                     this.messages.push(data)
@@ -39,5 +45,4 @@
 </script>
 
 <style>
-
 </style>
