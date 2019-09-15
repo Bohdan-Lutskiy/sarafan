47805e185b2108a739b490001a4349ae4aea65c3
Bohdan Lutskiy <47318375+bum240798@users.noreply.github.com>
2019-09-15 12:58:12 +0300

    Styling our app (#8)
    * Added Vuetify and was created beautiful view for our app

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,19 +1,28 @@
 <template>
-    <div>
-        <div v-if="!profile">You need login through
-            <a href="/login">Google</a>
-        </div>
-        <div v-else>
-            <div>{{profile.name}}&nbsp;<a href="/logout">Logout</a></div>
-            <messages-list :messages="messages"/>
-        </div>
-    </div>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title>Sarafan</v-toolbar-title>
+            <div class="flex-grow-1"></div>
+            <span v-if="profile">{{profile.name}}</span>
+            <v-btn v-if="profile" icon href="/logout">
+                <v-icon>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-content>
+            <v-container v-if="!profile">You need login through
+                <a href="/login">Google</a>
+            </v-container>
+            <v-container v-if="profile">
+                <messages-list :messages="messages"/>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
     import MessagesList from "components/messages/MessageList.vue";
-    import { addHandler } from "util/ws";
-    import { getIndex } from "util/collections";
+    import {addHandler} from "util/ws";
+    import {getIndex} from "util/collections";
 
     export default {
         components: {
@@ -28,7 +37,7 @@
         created() {
             addHandler(data => {
                 let index = getIndex(this.messages, data.id)
-                if (index > -1){
+                if (index > -1) {
                     this.messages.splice(index, 1, data)
                 } else {
                     this.messages.push(data)
@@ -39,5 +48,4 @@
 </script>
 
 <style>
-
 </style>
