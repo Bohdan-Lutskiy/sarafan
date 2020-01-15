48732c926d6009294816fd625457b56ebcb623fc
monst <monster240798@gmail.com>
2020-01-15 18:52:26 +0200

    Added Routing, few pages and route buttons

---
+++ src/main/resources/static/js/pages/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container v-if="!profile">You need login through
+        <a href="/login">Google</a>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Auth"
+    }
+</script>
+
+<style scoped>
+
+</style>
