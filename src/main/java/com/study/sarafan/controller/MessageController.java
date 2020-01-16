cf409a354e07d5ed72e3c4d2b9832082512bf692
Bohdan Lutskiy <47318375+bum240798@users.noreply.github.com>
2020-01-16 19:15:52 +0200

    Comments (#14)
    * Created Comment domain, repository, comments view, service and changed main and message controllers

--- src/main/java/com/study/sarafan/controller/MessageController.java
+++ src/main/java/com/study/sarafan/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.study.sarafan.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.study.sarafan.domain.Message;
+import com.study.sarafan.domain.User;
 import com.study.sarafan.domain.Views;
 import com.study.sarafan.dto.EventType;
 import com.study.sarafan.dto.MetaDto;
@@ -14,6 +15,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -54,9 +56,14 @@ public class MessageController {
     }
 
     @PostMapping
-    public Message create(@RequestBody Message message) throws IOException {
+    public Message create(
+            @RequestBody Message message,
+            @AuthenticationPrincipal User user
+    ) throws IOException {
         message.setCreationDate(LocalDateTime.now());
         fillMeta(message);
+        message.setAuthor(user);
+
         Message updatedMessage = messageRepo.save(message);
 
         wsSender.accept(EventType.CREATE, updatedMessage);
--- src/main/java/com/study/sarafan/repo/MessageRepo.java
+++ src/main/java/com/study/sarafan/repo/MessageRepo.java
@@ -1,9 +1,15 @@
 package com.study.sarafan.repo;
 
 import com.study.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepo extends JpaRepository<Message, Long> {
+
+    @EntityGraph(attributePaths = {"comments"})
+    List<Message> findAll();
 }
