2a153257695b6b819a99248a2d2679ab26660217
monst <monster240798@gmail.com>
2020-01-21 10:56:19 +0200

    Changed UI component package, added maven-yarn-plugin

--- src/main/java/com/study/sarafan/controller/MainController.java
+++ src/main/java/com/study/sarafan/controller/MainController.java
@@ -26,7 +26,7 @@ public class MainController {
     private final MessageService messageService;
     private final UserDetailsRepo userDetailsRepo;
 
-    @Value("${spring.profile.active}")
+    @Value("${spring.profile.active:prod}")
     private String profile;
     private final ObjectWriter messageWriter;
     private final ObjectWriter profileWriter;
